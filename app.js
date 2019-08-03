e6cd7c080be2550d5dad261ba8963e538f233aa7
victoria.unizhona <vikaunizhona@gmail.com>
2019-08-03 22:42:22 +0300

    added new explanatory comments

--- app.js
+++ app.js
@@ -22,20 +22,20 @@ function loadEventListeners() {
   filter.addEventListener("keyup", filterTasks);
 
   // DOM Load Event
-  document.addEventListener("DOMContentLoaded", getTasks);
+  document.addEventListener("DOMContentLoaded", getTasks); //The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 };
 
 // Get Tasks from LS
 
 function getTasks() {
   let tasks;
-  if (localStorage.getItem("tasks") === null) {
+  if (localStorage.getItem("tasks") === null) { //check by key
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem("tasks")); // returns array of values from the Local Storage and removes the quotes
   }
 
-  tasks.forEach(function (task) {
+  tasks.forEach(function (task) { // for each element from array of local storage
     // Create li element
     const li = document.createElement("li");
 
@@ -128,9 +128,10 @@ function storeTaskInLocalStorage(task) {
 
 // Remove task
 function removeTask(e) {
-  if (e.target.parentElement.classList.contains('delete-item')) {
+  if (e.target.parentElement.classList.contains('delete-item')) { //ul > li > a, we are talking about li here  The target event property returns the element that triggered the event.
+
     if (confirm("Are you sure?")) {
-      e.target.parentElement.parentElement.remove();
+      e.target.parentElement.parentElement.remove(); // li
 
       // Remove from LS
       removeTaskFromLocalStorage(e.target.parentElement.parentElement);
@@ -148,12 +149,12 @@ function removeTaskFromLocalStorage(tasksItem) {
   }
 
   tasks.forEach(function (task, index) {
-    if (tasksItem.textContent === task) {
-      tasks.splice(index, 1);
+    if (tasksItem.textContent === task) { //li content corresponds to the one we have in the Local Storage
+      tasks.splice(index, 1); //remove from to;
     }
   });
 
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks)); //The setItem() method of the Storage interface, when passed a key name and value, will add that key to the given Storage object, or update that key's value if it already exists.
 
 }
 
@@ -177,7 +178,8 @@ function filterTasks(e) {
 
   document.querySelectorAll(".collection-item").forEach(function (task) {
     const item = task.firstChild.textContent;
-    if (item.toLocaleLowerCase().indexOf(text) != -1) {
+    if (item.toLocaleLowerCase().indexOf(text) != -1) { // =1 means not found.  Returns the position of the first occurrence of a substring.
+
       task.style.display = "block";
     } else {
       task.style.display = "none";
